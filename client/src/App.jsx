9deef56c9156b5068ca73fd59cbf04830bc71cfc
Owen_Battles <owen.battles@icloud.com>
2025-12-13 10:44:40 -0800

    frontend set up with react + js

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './styles/App.css'
+
+function App() {
+  return (
+    <p1>Wiki-Race</p1>
+  )
+}
+
+export default App
